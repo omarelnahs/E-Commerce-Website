9a632a40c2651bf082391756d1676223095af3e3
omarelnahs <omarelnahas1141@email.com>
2023-10-08 07:24:23 +0300

    up

---
+++ src/Navigation/Nav.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { FiHeart } from "react-icons/fi";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <div className="nav-container">
+        <input
+          type="text"
+          placeholder="Enter Your Search shoes"
+          className="search-input"
+        />
+      </div>
+      <div className="profile-container">
+        <a href="#">
+          <FiHeart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons" />
+        </a>
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons" />
+        </a>
+      </div>
+    </nav>
+  );
+}
+export default Nav;
